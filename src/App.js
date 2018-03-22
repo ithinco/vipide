1f43cf1ac7233eb172d5b0a024b271253bedf585
Guan Ce <ithinco@gmail.com>
2018-03-22 13:51:52 +0800

    Init

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+class App extends Component {
+  render () {
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <h1 className='App-title'>This site is under construction</h1>
+          <h1 className='App-title'>网站正在建设中</h1>
+        </header>
+      </div>
+    )
+  }
+}
+
+export default App
